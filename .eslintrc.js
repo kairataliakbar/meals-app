97d513601ba9f1088843d8a62d6f70905448f7fc
kakeKrg <aliakbar.kairat@gmail.com>
2020-07-12 21:29:23 +0600

    install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+};
