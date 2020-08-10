353232e61421a024574263e8c9bac7b4c8f77ab2
kakeKrg <aliakbar.kairat@gmail.com>
2020-08-11 00:43:58 +0600

    realization filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,23 +1,84 @@
 /* eslint-disable react/display-name */
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import GlobalStyles from "../constants/GlobalStyles";
+import Colors from "../constants/Colors";
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center"
+  },
+  filterContainer: {
+    flexDirection: "row",
+    width: "80%",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center"
+    marginVertical: 10
   }
 });
 
-const FiltersScreen = () => (
-  <View style={styles.screen}>
-    <Text>The Filters Screen!</Text>
+const InputSwitch = ({ label, value, onChange }) => (
+  <View style={styles.filterContainer}>
+    <Text>{label}</Text>
+    <Switch
+      value={value}
+      onValueChange={onChange}
+      trackColor={{ true: Colors.primaryColor }}
+      thumbColor={Platform.OS === "android" && Colors.primaryColor}
+    />
   </View>
 );
+
+InputSwitch.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+const FiltersScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  return (
+    <View style={styles.screen}>
+      <Text style={{ ...styles.title, ...GlobalStyles.customTextBold }}>
+        Available Filters / Restrictions
+      </Text>
+      <InputSwitch
+        label="Gluten-free"
+        value={isGlutenFree}
+        onChange={() => setIsGlutenFree(!isGlutenFree)}
+      />
+      <InputSwitch
+        label="Lactose-free"
+        value={isLactoseFree}
+        onChange={() => setIsLactoseFree(!isLactoseFree)}
+      />
+      <InputSwitch
+        label="Vegan"
+        value={isVegan}
+        onChange={() => setIsVegan(!isVegan)}
+      />
+      <InputSwitch
+        label="Vegetarian"
+        value={isVegetarian}
+        onChange={() => setIsVegetarian(!isVegetarian)}
+      />
+    </View>
+  );
+};
 
 FiltersScreen.navigationOptions = ({ navigation }) => {
   return {
